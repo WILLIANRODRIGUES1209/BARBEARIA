2b2f46db353da84431391e713f7489792059c644
WILLIAN GESTÃO PRO <willianfreitas944@gmail.com>
2026-05-28 21:36:21 -0300

    AGORA VAI

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-<<<<<<< HEAD
 import { registerSW } from 'virtual:pwa-register';
 
-// Register service worker
 if ('serviceWorker' in navigator) {
   registerSW({ immediate: true });
 }
-=======
->>>>>>> f67eaa8a2ae6d296261c137d544b1b6f6bcb6798
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
